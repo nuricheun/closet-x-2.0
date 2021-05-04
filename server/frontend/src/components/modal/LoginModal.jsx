009df2b24859bb6164f943cc5248c651cb0e4069
aliglaser <nooti777712@gmail.com>
2021-05-04 02:07:25 -0700

    session auth

--- server/frontend/src/components/modal/LoginModal.jsx
+++ server/frontend/src/components/modal/LoginModal.jsx
@@ -1,6 +1,7 @@
 //MODAL DESIGN INSPIRED BY: https://codepen.io/alligatorio/pen/aYzMKL
 import {useState} from "react";
 import {useModalRef} from '../../hooks/useModal';
+import { authValidation } from '../../util/authValidation';
 import './modal.css';
 
 const LoginModal = ({login, hideModal}) => {
@@ -12,13 +13,24 @@ const LoginModal = ({login, hideModal}) => {
       email: "",
       password: ""
     })
+
+  const [error, setError] = useState({});
     
   const handleInput = (type) => {
     return e => setState({ ...state, [type]: e.target.value });
   }
 
+  const handleClick = (type) => { 
+    return e => setError({ ...error, [type]: undefined });
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const res = authValidation(state, "signin")
+    if(res[0] !== true){
+      setError({...error, ...res[1]})
+      return
+    }
     login(state).then(() => hideModal());
   }
 
@@ -32,34 +44,35 @@ const LoginModal = ({login, hideModal}) => {
   }
 
     return (
-
-          <div className="form-container" ref={node}>
-            <div className="form-header">Login</div>
+      <div className="form-container" ref={node}>
+        <div className="form-header">Login</div>
+        <br />
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email:
             <br />
-            <form onSubmit={handleSubmit}>
-              <label>
-                Email:
-                <br />
-                <input type="text" onChange={handleInput("email")} />
-              </label>
-              <br />
-              <label>
-                Password:
-                <br />
-                <input
-                  type="password"
-                  onChange={handleInput("password")}
-                />
-              </label>
-              <br />
-              <div className="button-container">
-                <button className="modal-button" onClick={handleSubmit}>Log In</button>
-                <button className="modal-button" onClick={handleDemoLogin}>Demo Login</button>
-              </div>
-            </form>
+            <input type="text" onChange={handleInput("email")} onClick={handleClick('email')}/>
+            { error["email"] && <p className="error-message">{error["email"]}</p>}
+          </label>
+          <br />
+          <label>
+            Password:
+            <br />
+            <input
+              type="password"
+              onChange={handleInput("password")}
+              onClick={handleClick('password')}
+            />
+            { error["password"] && <p className="error-message">{error["password"]}</p>}
+          </label>
+          <br />
+          <div className="button-container">
+            <button className="modal-button" onClick={handleSubmit}>Log In</button>
+            <button className="modal-button" onClick={handleDemoLogin}>Demo Login</button>
           </div>
+        </form>
+      </div>
     );
-  
 }
 
 export default LoginModal;
--- server/frontend/src/components/modal/SignupModal.jsx
+++ server/frontend/src/components/modal/SignupModal.jsx
@@ -2,6 +2,7 @@
 
 import {useState} from "react";
 import {useModalRef} from '../../hooks/useModal';
+import { authValidation } from '../../util/authValidation';
 import './modal.css';
 
 const SignupModal = ({signup, hideModal}) => {
@@ -17,11 +18,22 @@ const SignupModal = ({signup, hideModal}) => {
     return e => setUserInfo({ ...userinfo, [type]: e.target.value });
   }
 
+  const [error, setError] = useState({});
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const res = authValidation(userinfo, "notsignin")
+    if(res[0] !== true){
+      setError({...error, ...res[1]})
+      return;
+    }
     signup(userinfo).then(()=>hideModal());
   }
 
+
+  const handleClick = (type) => { 
+    return e => setError({ ...error, [type]: undefined });
+  }
     const node = useModalRef(hideModal)
 
     return (
@@ -32,13 +44,15 @@ const SignupModal = ({signup, hideModal}) => {
               <label>
                 Username:
                 <br />
-                <input type="text" onChange={handleInput("username")} />
+                <input type="text" onChange={handleInput("username")} onClick={handleClick('username')}/>
+                { error["username"] && <p className="error-message">{error["username"]}</p>}
               </label>
               <br />
               <label>
                 Email:
                 <br />
-                <input type="text" onChange={handleInput("email")} />
+                <input type="text" onChange={handleInput("email")} onClick={handleClick('email')}/>
+                { error["email"] && <p className="error-message">{error["email"]}</p>}
               </label>
               <br />
               <label>
@@ -47,7 +61,9 @@ const SignupModal = ({signup, hideModal}) => {
                 <input
                   type="password"
                   onChange={handleInput("password")}
+                  onClick={handleClick('password')}
                 />
+                { error["password"] && <p className="error-message">{error["password"]}</p>}
               </label>
               <br />
               <label>
@@ -56,7 +72,9 @@ const SignupModal = ({signup, hideModal}) => {
                 <input
                   type="password"
                   onChange={handleInput("password2")}
+                  onClick={handleClick('passwordconfirm')}
                 />
+                { error["passwordconfirm"] && <p className="error-message">{error["passwordconfirm"]}</p>}
               </label>
               <br />
               <div className="button-container">
