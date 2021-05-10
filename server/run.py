1a2ac751620d188fa4f271c55a4b780f1414754f
aliglaser <nooti777712@gmail.com>
2021-05-10 11:12:51 -0700

    if name == main

--- server/run.py
+++ server/run.py
@@ -2,4 +2,6 @@ from app import create_app
 # from extensions import config
 
 app = create_app()
-app.run()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
