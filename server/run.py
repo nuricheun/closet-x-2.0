9309e344c4e8f50c2c56813ff2449f582f653099
aliglaser <nooti777712@gmail.com>
2021-05-10 13:08:08 -0700

    del favicon

--- server/run.py
+++ server/run.py
@@ -4,4 +4,4 @@
 if __name__ == "__main__":
     from app import create_app
     app = create_app()
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
