7b260bf7a36df5f873a82f6d09296279c8e01c4d
aliglaser <nooti777712@gmail.com>
2021-05-10 14:34:58 -0700

    move run to the name==main block

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 from server.app import create_app
 app = create_app()
-app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 5000))
 
 # from extensions import config
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 5000))
