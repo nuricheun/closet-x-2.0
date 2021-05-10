3f87813856ed34d58bf8f34a157e7a7eb2af0913
aliglaser <nooti777712@gmail.com>
2021-05-10 13:13:56 -0700

    file moved

---
+++ run.py
@@ -0,0 +1,7 @@
+
+
+# from extensions import config
+if __name__ == "__main__":
+    from ./server/app import create_app
+    app = create_app()
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 5000))
--- server/run.py
+++
@@ -1,7 +0,0 @@
-
-
-# from extensions import config
-if __name__ == "__main__":
-    from app import create_app
-    app = create_app()
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
