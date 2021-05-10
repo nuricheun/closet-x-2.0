97955b3cafab5354b5ff4b5cac9cea99fbaca77f
aliglaser <nooti777712@gmail.com>
2021-05-10 11:17:23 -0700

    fix

--- server/run.py
+++ server/run.py
@@ -1,7 +1,7 @@
-from app import create_app
+
+
 # from extensions import config
-
-app = create_app()
-
 if __name__ == "__main__":
+    from app import create_app
+    app = create_app()
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
